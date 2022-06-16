923055411b80b84a7ad4bcf87df5d94f4202ff80
malickbax <malickbax@gmail.com>
2022-06-16 12:52:27 -0400

    adjusted body font using Google Fonts

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,21 @@
-document.getElementById("currentDay").innerHTML = new Date
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, text);
+    })
+   
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+})
+
+console.log(ready)
