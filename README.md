03f4fde788de7ab646bcde8513a855cba546c2f7
malickbax <malickbax@gmail.com>
2022-06-19 15:10:32 -0400

    added functions in JS file to save user inputs locally. Converted hours to AM/PM format

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # workday-planner
-A simple calendar application that allows users to save events for each hour of the day. This app runs  in the browser and features dynamically updated HTML and CSS powered by jQuery.
+Work Day Scheduler is a simple calendar application that allows users to save events for each hour of the day. This app runs  in the browser and features dynamically updated HTML and CSS powered by jQuery.
+- Deployed URL: https://malickbax.github.io/workday-planner/
+- GitHub Repo URL: https://github.com/malickbax/workday-planner
--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,46 @@
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 
 $(document).ready(function () {
+    // When user clicks on the save icon, content of the "description" section is saved locally
     $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, text);
     })
-   
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
+
+// Recover items saved in the lcoal storage
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
 })
 
-console.log(ready)
+// Function for conditional highliting
+function time() {
+    var timeNow = moment().hour();
+    $(".timeBlock").each(function () {
+        var timeNow = parseInt($(this).attr("id"));
+// Compare current time with time shown on schedule
+        if (timeBlock < timeNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (timeBlock === timeNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+time();
